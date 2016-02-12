aa25b3879b0871eaafe7eb93be925f2475520579
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-02-12 16:03:58 +0100

    outliers lesson

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -27,14 +27,12 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### the plotting code below works, and you can see what your regression looks like
 
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
+print 'slope: ', reg.coef_
+print 'score: ', reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -78,7 +76,9 @@ if len(cleaned_data) > 0:
     plt.ylabel("net worths")
     plt.show()
 
+    print 'slope[new]: ', reg.coef_
+    print 'score[new]: ', reg.score(ages_test, net_worths_test)
+
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
