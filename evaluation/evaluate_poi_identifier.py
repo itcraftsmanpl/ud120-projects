a21ae5ce6d3fb07a22fab5a4c4735c5f4b51d0dd
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-02-26 15:03:51 +0100

    evaluate metrics lesson

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,34 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from time import time
+from sklearn import cross_validation
 
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+t0 = time()
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+predictions = classifier.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+#print predictions
+#print labels_test
+
+accuracy = accuracy_score(labels_test, predictions)
+print("Accuracy: {}".format(accuracy))
+
+precision = precision_score(labels_test, predictions)
+print("Precision: {}".format(precision))
+
+recall = recall_score(labels_test, predictions)
+print("Recall: {}".format(recall))
+
+print("#Recall: {}".format(recall_score([0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0], [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1])))
